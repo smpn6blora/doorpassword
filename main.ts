4a86cb4268b86e96b29da2649966d699e550f9cd
smpn6blora <109122900+smpn6blora@users.noreply.github.com>
2022-11-08 00:11:14 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    entry = "" + entry + "A"
+})
+input.onButtonPressed(Button.AB, function () {
+    if (password == entry) {
+        basic.showIcon(IconNames.Yes)
+        pins.servoWritePin(AnalogPin.P0, 90)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(500)
+    basic.clearScreen()
+    entry = ""
+})
+input.onButtonPressed(Button.B, function () {
+    entry = "" + entry + "B"
+})
+let entry = ""
+let password = ""
+password = "AABBAB"
+entry = ""
+pins.servoWritePin(AnalogPin.P0, 0)
